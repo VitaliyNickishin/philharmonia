0ccb44b78efaa24869cbd02dd73b7d19d5fb1ae8
“VitaliyNickishin” <nickishin34@gmail.com>
2019-11-09 16:54:04 +0200

    mark up Header is ready

---
+++ src/components/Header/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import logo from '../../logo.png'
+import search from '../../assets/img/icons/search.png'
+import {Container} from 'react-bootstrap'
+import SocialIcons from '../SocialIcons/SocialIcons'
+import './Header.sass'
+
+export default function Header() {
+ return (
+  <header className="header">
+   <Container>
+    <div className="header-top">
+     <div className="logo">
+      <img src={logo} alt="logo"/>
+     </div>
+
+     <div className="date header-top__begining">
+      <h1>2017</h1>
+      <span>season</span>
+     </div>
+
+     <div className="date header-top__finish">
+      <h1 className="end">2018</h1>
+      <span className="end">season</span>
+     </div>
+
+     <div className="text header-top__text">
+      <p>Sydney philharmonia choirs  
+       <span> celebrating 100 years in 2020</span>
+      </p>
+     </div>
+
+     <div className="right-info">
+      <div className="right-info__text">
+       shop/<br/>cart
+      </div>
+      <div className="right-info__icons">
+       <SocialIcons />
+       <button type="button">
+        <img src={search} alt="search"/>
+       </button>
+      </div>
+       <a href="#" className="right-info__donate">
+        <p>donate</p>
+       </a>
+
+     </div>
+     
+    </div>{/* header-top */}
+    
+   </Container>
+   
+
+
+   
+  </header>
+ )
+}
---
+++ src/components/SocialIcons/SocialIcons.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import fb from '../../assets/img/social/fb.png'
+import tw from '../../assets/img/social/tw.png'
+import inst from '../../assets/img/social/inst.png'
+import youtube from '../../assets/img/social/youtube.png'
+import './SocialIcons.sass'
+
+export default function SocialIcons({name}) {
+ return (
+  <div className={name}>
+   <a href="#">
+    <img src={fb} alt="fb"/>
+   </a>
+   <a href="#">
+    <img src={tw} alt="tw"/>
+   </a>
+   <a href="#">
+    <img src={inst} alt="inst"/>
+   </a>
+   <a href="#">
+    <img src={youtube} alt="youtube"/>
+   </a>
+  </div>
+ )
+}
+
+SocialIcons.defaultProps={
+ name: 'social'
+}
